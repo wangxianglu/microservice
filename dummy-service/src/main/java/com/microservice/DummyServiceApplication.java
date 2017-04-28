23c3093fc1e62dc37192097463dbedfd0a37f831
wangxianglu <lu_action@qq.com>
2017-04-28 17:04:09 +0800

    add other moudle

---
+++ dummy-service/src/main/java/com/microservice/DummyServiceApplication.java
@@ -0,0 +1,12 @@
+package com.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyServiceApplication.class, args);
+	}
+}
